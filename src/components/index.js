83f74d13a052c2bb816050d604a1bc233435a817
Divyanshuk09 <123636880+Divyanshuk09@users.noreply.github.com>
2025-01-15 16:24:13 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/Container";
+import Button from "./Button";
+import RTE from "./RTE";
+import Input from "./Input";
+import Select from "./Select";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+export  {
+    Header,
+    Footer,
+    LogoutBtn,
+    Container,
+    Button,
+    RTE,
+    Input,
+    Select,
+    SignUp,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout
+}
